7816a1f94348efce541de07bb457abe89da52e27
qlcx <1136425019@qq.com>
2016-07-19 22:51:32 +0800

    imgur image

--- Imgur/src/components/topic.jsx
+++ Imgur/src/components/topic.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var Actions = require('../actions');
+var ImageStore = require('../stores/image-store');
+var Reflux = require('reflux');
 
 module.exports = React.createClass({
+    mixins: [
+        Reflux.listenTo(ImageStore, 'onChange')
+    ],
+
+    getInitialState: function() {
+        return {
+            images: []
+        }
+    },
+
+    componentWillMount: function() {
+        Actions.getImages(this.props.params.id);
+    },
+
+    onChange: function(event, images) {
+        this.setState({images: images});
+    },
+
     render: function() {
         return(
             <div>
-             I am a topic!
+
             </div>
         );
     }
